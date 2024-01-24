f7fa6dbcd4b871c52e9b955f2d0c4e1e481b40a0
ppavic <ppavic@gmail.com>
2024-01-24 19:57:36 +0100

    fibb

---
+++ fibb.php
@@ -0,0 +1,63 @@
+<?php
+
+
+function fib(int $n){
+
+
+    if($n === 1){
+        return 1;
+    }
+    elseif($n === 0){
+        return 0;
+    }
+    else{
+        return fib($n-1) + fib($n-2);
+    }
+}
+
+
+
+function show_fibbonaci($n){
+
+    for($i=0; $i<$n; $i++){
+        echo fib($i).PHP_EOL;
+    }
+
+}
+
+//show_fibbonaci(20);
+
+
+function check_palindrom(string $text){
+   
+    $result = 'Palindrom! :)'.PHP_EOL;
+        for($n=0; $n<strlen($text); $n++){
+
+        if(!($text[$n] === $text[strlen($text)-1-$n])){
+            $result = 'Nije palindrom';
+        }
+
+        return $result;
+    }
+}
+
+echo check_palindrom('anavolimilovana');
+
+
+function checkSum(){
+    $niz = [];
+$a = 1;
+    while ($a <= 100) {
+       $niz[] = readline('unesite broj: ');
+       $a++;
+    }
+    echo 'suma: '.array_sum($niz).PHP_EOL;
+    if (array_sum($niz)%2 === 1){
+        checkSum();
+    }
+    else{
+        echo 'najveći broj niza je: '. max($niz). PHP_EOL;
+    }
+}
+
+checkSum();
